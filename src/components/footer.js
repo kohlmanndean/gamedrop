884f98810921f39c54931ebb6f7b797bd3492fb4
kohlmanndean <61201957+kohlmanndean@users.noreply.github.com>
2022-02-08 18:18:24 -0800

    use fontawesome

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+
 export default function Footer() {
 	return (
 		<footer className='max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8 flex justify-center space-x-4'>
-			<a href='/' className=''>
-				<img src='/github-logo.png' alt='Github' className='h-8 w-auto' />
+			<a href='https://github.com/andrepn/gamedrop' target='_blank' rel='noreferrer'>
+				<FontAwesomeIcon icon={brands('github')} alt='Github' className='h-6 w-auto text-day' />
 			</a>
-			<a href='/' className=''>
-				<img src='/twitter-logo.png' alt='Twitter' className='h-8 w-auto' />
+			<a href='/' target='_blank' rel='noreferrer'>
+				<FontAwesomeIcon icon={brands('twitter')} alt='Twitter' className='h-6 w-auto text-day' />
+			</a>
+			<a href='https://discord.gg/F7UdDchd' target='_blank' rel='noreferrer'>
+				<FontAwesomeIcon icon={brands('discord')} alt='Discord' className='h-6 w-auto text-day' />
 			</a>
 		</footer>
 	)
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,8 @@
 import { Disclosure } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import { shortenAddress, useEthers } from '@usedapp/core'
 import { Link, useMatch, useResolvedPath } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 function classNames(...classes) {
 	return classes.filter(Boolean).join(' ')
@@ -19,7 +20,7 @@ export default function Header({ routes }) {
 									{/* Mobile menu button */}
 									<Disclosure.Button className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
 										<span className='sr-only'>Open main menu</span>
-										{open ? <XIcon className='block h-6 w-6' aria-hidden='true' /> : <MenuIcon className='block h-6 w-6' aria-hidden='true' />}
+										<FontAwesomeIcon icon={open ? solid('times') : solid('bars')} className='block h-6 w-6 text-day' aria-hidden='true' />
 									</Disclosure.Button>
 								</div>
 								<div className='flex-shrink-0 flex items-center space-x-1'>
